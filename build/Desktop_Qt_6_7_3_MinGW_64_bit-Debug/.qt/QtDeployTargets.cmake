ba926d569a61b25b72e59ba92701db45457c5a16
DGHaZe <34streets@gmail.com>
2024-10-13 16:14:16 +0300

    add files

---
+++ build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_appHome_work5_Cpro_FILE E:/cod/Home_work5_Cpro/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/appHome_work5_Cpro.exe)
+set(__QT_DEPLOY_TARGET_appHome_work5_Cpro_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_appHome_work5_Cpro_RUNTIME_DLLS C:/Qt/6.7.3/mingw_64/bin/Qt6Quick.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6QmlModels.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Qml.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Network.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Core.dll)
